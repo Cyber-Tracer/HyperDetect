b675ecf635d1d0169405cc08519cfd73b95295df
lj <janluethi2000@gmail.com>
2024-04-22 18:01:37 +0200

    WIP basic Pi setup

---
+++ controller/setup/setup.sh
@@ -0,0 +1,25 @@
+# This script is used to setup the controller node
+# It is assumed the controller node is still connected to the internet
+# requires to be run with sudo
+
+# Update and upgrade the system
+apt update
+apt upgrade
+reboot
+
+# Add logger user
+adduser logger
+cd /home/logger
+git clone https://github.com/Cyber-Tracer/HyperDetect.git
+
+# IP address of the controller node
+cp ./01-network-manager-all.yaml /etc/netplan/01-network-manager-all.yaml
+netplan apply
+
+# Cron task to start log server on reboot
+
+# Restrict firewall to only allow connections from the logger nodes
+ufw default deny incoming
+ufw allow 8989/tcp
+ufw enable
+
