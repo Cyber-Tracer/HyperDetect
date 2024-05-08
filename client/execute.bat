32fd39eeda1dde1955b9e7e3b98c732dfa14efbb
lj <janluethi2000@gmail.com>
2024-05-08 07:59:55 +0200

    clean up

--- client/execute.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-timeout /t 10 /nobreak && C:\HyperDbg\client\Malware\RansomwarePOC.exe -p C:\Users\Client\Documents -e
-pause
--- controller/setup/setup.sh
+++ controller/setup/setup.sh
@@ -21,6 +21,7 @@ netplan apply
 # Restrict firewall to only allow connections from the logger nodes
 ufw default deny incoming
 ufw allow 8989/tcp
+ufw allow 9090/tcp
 ufw enable
 
 # Mount usb drive
