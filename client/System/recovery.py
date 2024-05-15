e5f12c8c9c3234fa32e0b41b1fba029d76b05e3c
lj <janluethi2000@gmail.com>
2024-05-15 09:42:29 +0200

    Add file recovery

---
+++ client/System/recovery.py
@@ -0,0 +1,70 @@
+import subprocess
+import os
+import shutil
+
+BACKUP_TARGET = 'D:'
+FILE_BACKUP_PATH = 'D:\\FileBackup'
+RECOVERY_PARENT_DIR = 'C:\\Users\\Client'
+
+def get_version_information():
+    """
+    use wbadmin to get the version information of the backup
+
+    Returns:
+        str: Backup time
+        str: Backup target
+        str: Version identifier
+        str: What backup can recover
+        str: Snapshot ID
+    
+    """
+    cmd_list = ['wbadmin', 'get', 'versions', f'-backupTarget:{BACKUP_TARGET}']
+    result = subprocess.run(cmd_list, capture_output=True, text=True)
+    
+    # Check if the command was successful
+    if result.returncode != 0:
+        raise ValueError("Error listing backup versions")
+    
+    # Parse the output to find the first available backup version
+    output_lines = result.stdout.split('\n')[3:8]
+    output_lines = [line.split(': ')[1] for line in output_lines]
+    
+    return output_lines
+
+def recover_files():
+    """
+    Recover files from FILE_BACKUP_PATH to DIRS_TO_RECOVER
+    """
+    zips = [f for f in os.listdir(FILE_BACKUP_PATH) if f.endswith('.zip')]
+    for zip_file in zips:
+        recovery_dir = os.path.join(RECOVERY_PARENT_DIR, os.path.splitext(zip_file)[0])
+        if not os.path.exists(recovery_dir):
+            print(f"{recovery_dir} not found, creating...")
+            os.makedirs(recovery_dir)
+        shutil.rmtree(recovery_dir)
+        os.makedirs(recovery_dir)
+        shutil.unpack_archive(os.path.join(FILE_BACKUP_PATH, zip_file), recovery_dir)
+        print(f"Recovered {zip_file} to {recovery_dir}")
+
+
+def recover(mode):
+    """
+        Recover the system using the specified mode
+        supported modes: 
+            "client_files" - recover CLIENT_FILES_PATH directory, does not require restart
+            "volume" - recover the entire volume, requires restart
+    """
+
+    match mode:
+        case "client_files":
+            print(f"Recovering files from {FILE_BACKUP_PATH}...")
+            recover_files()
+            print("File recovery complete.")
+        case "volume":
+            backup_time, _, version_id, can_recover, _ = get_version_information()
+            print("Not implemented yet")
+        case _:
+            raise ValueError("Invalid recovery mode")
+        
+if __name__ == '__main__':
+    recover("client_files")
