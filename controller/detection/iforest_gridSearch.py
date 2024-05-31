758ca028aeba35c5aa6262442a90bc691a27ebe8
lj <janluethi2000@gmail.com>
2024-05-31 10:15:51 +0200

    conduct gridsearch across ngrams

--- controller/detection/iforest_gridSearch.py
+++ controller/detection/iforest_gridSearch.py
@@ -2,8 +2,8 @@ from pyod.models.iforest import IForest
 from sklearn.metrics import f1_score
 from log_reader import read_all_logs
 from preprocessors.preprocessor import Preprocessor
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
+from train_models import train_test_split_df
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 import numpy as np
 import itertools
 from concurrent.futures import ThreadPoolExecutor, as_completed
@@ -14,24 +14,24 @@ df = read_all_logs(version=1)
 preprocessor = Preprocessor.get(version=1)
 df = preprocessor.preprocess(df)
 
-# Vectorize data
-vectorizer = CountVectorizer(ngram_range=(3, 3))
-X = vectorizer.fit_transform(df['syscall']).toarray()
-y = df['malicious']
-
 # Split the data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-X_test = np.concatenate((X_test, X_train[y_train == 1]))
-y_test = np.concatenate((y_test, y_train[y_train == 1]))
+X_train, X_test, y_train, y_test = train_test_split_df(df)
 X_train = X_train[y_train == 0]
 y_train = y_train[y_train == 0]
 
 # Define the parameters to be tuned
+
+ngram_range = range(1, 6)
+vectorizers = [CountVectorizer, TfidfVectorizer]
+
+
+# Define the parameters to be tuned
 param_grid = {
+    'ngram': ngram_range,
+    'vectorizer': vectorizers,
     'n_estimators': [50, 100, 200],
-    'max_samples': ['auto', 0.5, 0.75, 1.0],
+    'max_samples': ['auto', 0.5, 1.0],
     'max_features': [0.5, 0.75, 1.0],
-    'behaviour': ['old', 'new']
 }
 
 # Get all parameter names and their corresponding values
@@ -41,22 +41,35 @@ param_combinations = [dict(zip(keys, v)) for v in itertools.product(*values)]
 print(f"Total number of parameter combinations: {len(param_combinations)}")
 
 best_params = None
+best_scaler = None
+best_ngram = 0
 best_f1 = 0
 
+def vectorize(X_train, X_test, vectorizer):
+    X_train = vectorizer.fit_transform(X_train).toarray()
+    X_test = vectorizer.transform(X_test).toarray()
+    return X_train, X_test
+
 def train_and_evaluate(params):
+    vectorizer = params.pop('vectorizer')
+    ngram = params.pop('ngram')
+    vectorizer = vectorizer(ngram_range=(ngram, ngram), token_pattern=r'\b\w+\b')
+    X_train_vectorized, X_test_vectorized = vectorize(X_train, X_test, vectorizer)
     model = IForest(contamination=0.044, random_state=42, **params)
-    model.fit(X_train)
-    y_test_pred = model.predict(X_test)
+    model.fit(X_train_vectorized)
+    y_test_pred = model.predict(X_test_vectorized)
     f1 = f1_score(y_test, y_test_pred)
-    return f1, params
+    return f1, params, vectorizer.ngram_range[0], vectorizer.__class__.__name__
 
 def update_best_params(results):
-    global best_f1, best_params
-    f1, params = results
-    print(f"Parameters: {params}, F1 Score: {f1}")
+    global best_f1, best_params, best_ngram, best_scaler
+    f1, params, ngram, scaler = results
+    print(f"Scaler: {scaler}, Ngram: {ngram}, Parameters: {params}, F1 Score: {f1}")
     if f1 > best_f1:
         best_f1 = f1
         best_params = params
+        best_ngram = ngram
+        best_scaler = scaler
 print("Starting grid search...")
 start = time.time()
 
@@ -72,3 +85,5 @@ with ThreadPoolExecutor(max_workers=16) as executor:
 print(f"Grid search completed in {time.time() - start} seconds")
 print(f"Best parameters found: {best_params}")
 print(f"Best F1 Score found: {best_f1}")
+print(f"Best Ngram found: {best_ngram}")
+print(f"Best Scaler found: {best_scaler}")
