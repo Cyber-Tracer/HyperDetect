c146484c43e5f985d192d620900e8b5d258595b7
lj <janluethi2000@gmail.com>
2024-06-12 17:20:01 +0200

    correct wrong flag

--- controller/input/V3-1/benign_zip-backup_30min/execute.bat
+++ controller/input/V3-1/benign_zip-backup_30min/execute.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d "%~dp0"
-"C:\Program Files\7-zip\7z.exe" -a -tzip  "D:\FileBackup\Documents_tst.zip" "C:\Users\Client\Documents\"
+"C:\Program Files\7-zip\7z.exe" a -tzip  "D:\FileBackup\Documents_tst.zip" "C:\Users\Client\Documents\"
