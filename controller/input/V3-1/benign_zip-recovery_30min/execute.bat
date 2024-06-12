376ccb57a90accb087465a9a72ed92c0c8e182d5
lj <janluethi2000@gmail.com>
2024-06-12 19:32:51 +0200

    Correct filepath

--- controller/input/V3-1/benign_zip-recovery_30min/execute.bat
+++ controller/input/V3-1/benign_zip-recovery_30min/execute.bat
@@ -6,5 +6,5 @@ REM Remove all subdirectories and their contents in the destination directory
 for /d %%x in ("C:\Users\Client\Documents\*") do @rd /s /q "%%x"
 
 REM Extract the zip file using PowerShell
-powershell -command "Expand-Archive -Path 'D:\Documents_tst.zip' -DestinationPath 'C:\Users\Client\Documents' -Force"
-del /q "D:\Documents_tst.zip"
+powershell -command "Expand-Archive -Path 'D:\FileBackup\Documents_tst.zip' -DestinationPath 'C:\Users\Client\Documents' -Force"
+del /q "D:\FileBackup\Documents_tst.zip"
