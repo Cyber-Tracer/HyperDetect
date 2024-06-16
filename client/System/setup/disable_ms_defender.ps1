1d1222f03a6adc46082c8d1e1fc823b9d798d259
lj <janluethi2000@gmail.com>
2024-06-16 15:12:32 +0200

    add powersettings and disable windows defender to setup

---
+++ client/System/setup/disable_ms_defender.ps1
@@ -0,0 +1,18 @@
+# Turn off real-time protection
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" -Name "DisableRealtimeMonitoring" -Value 1 -Type DWord
+
+# Turn off routine remediation
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "DisableRoutinelyTakingAction" -Value 1 -Type DWord
+
+# Disable Windows Defender services
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "DisableAntiSpyware" -Value 1 -Type DWord
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "DisableAntiVirus" -Value 1 -Type DWord
+
+# Disallow antimalware service to remain running always
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\WinDefend" -Name "Start" -Value 2 -Type DWord
+
+# Stop Windows Defender services if running
+Stop-Service -Name WinDefend -Force -ErrorAction SilentlyContinue
+Stop-Service -Name WdNisSvc -Force -ErrorAction SilentlyContinue
+
+Write-Host "Windows Defender has been disabled."
---
+++ client/System/setup/setup_power_settings.ps1
@@ -0,0 +1,7 @@
+# Disable screen turn off
+powercfg /change monitor-timeout-ac 0
+powercfg /change monitor-timeout-dc 0
+
+# Disable sleep
+powercfg /change standby-timeout-ac 0
+powercfg /change standby-timeout-dc 0
